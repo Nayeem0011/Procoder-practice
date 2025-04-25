2dfdb69fe24d61e78c9c3c4c25ebfdd870f80d72
Sk Nayeem <sknayeemislam384@gmail.com>
2025-04-25 23:07:46 +0600

    array_change

--- array.js
+++ array.js
@@ -24,7 +24,126 @@
 // console.log(arr[2]);
 
 
-let arr = ["Nayeem", "Omor", "Adnan", "Nazmul", "Neloy"]
-console.log(arr[arr.length -1]); //Last item
+
+// let arr = ["Nayeem", "Omor", "Adnan", "Nazmul", "Neloy"]
+// console.log(arr[arr.length -1]); //Last item
+
+
+
+// let arr = ["Nayeem", "Omor", "Adnan", "Nazmul", "Neloy"]
+// console.log(arr[0], arr[1], arr[2]);
+
+
+
+
+// Looping over an Array
+
+
+// let herose = ["ironman", "thor", "hulk", "spiderman", "shaktiman"];
+// console.log(herose);
+
+
+//Useing for loop
+// let herose = ["ironman", "thor", "hulk", "spiderman", "shaktiman"];
+// for (let i = 0; i < herose.length; i++) {
+//     console.log(herose[i]);
+// }
+
+
+// let herose = ["ironman", "thor", "hulk", "spiderman", "shaktiman"];
+// for (let i = 1; i <= herose.length; i++) {
+//     console.log(herose[i-1]);
+// }
+
+
+
+//Useing for of loop
+// let herose = ["ironman", "thor", "hulk", "spiderman", "shaktiman"];
+// for (let hero of herose) {
+//     console.log(hero);
+// }
+
+
+
+
+// Practice Qs. For a given array with marks of students -> [85, 97, 44, 37, 76, 60]
+
+// let marks = [85, 97, 44, 37, 76, 60];
+// let sum = 0;
+// for (let val of marks) {
+//     sum += val;
+// }
+
+// let avg = sum / marks.length;
+// console.log(`Avg marks of the class = ${avg}`);
+
+
+// Practice Qs. For a given array with prices of 5 items -> [250, 645, 300, 900, 50]
+
+// Useing for of loop
+
+// let items = [250, 645, 300, 900, 50]
+// let i = 0; 
+// for (let val of items) {
+//     console.log(`Value at index ${i} = ${val}`);
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`Value after offer = ${items[i]}`);
+//     i++;
+// }
+
+
+
+// let items = [250, 645, 300, 900, 50]
+// let i = 0; 
+// for (let val of items) {
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`Value after offer = ${items[i]}`);
+//     i++;
+// }
+
+// Useing for loop
+
+// let items = [250, 645, 300, 900, 50]
+
+// for (let i = 0; i < items.length; i++) {
+//     let offer = items[i] / 10;
+//     items[i] -= offer;
+// }
+
+// console.log(items);
+
+
+
+//Array Methods 
+
+// let foodItems = ["Potato", "Apple", "Litchi", "tomato",]
+
+// foodItems.push("Chips", "burger")
+// console.log(foodItems);
+
+
+// let foodItems = ["Potato", "Apple", "Litchi", "tomato",];
+// foodItems.pop()
+// console.log(foodItems);
+
+
+
+// let foodItems = ["Potato", "Apple", "Litchi", "tomato",];
+// let deletedItem = foodItems.pop()
+// foodItems.pop()
+// console.log("deleted :", deletedItem);
+
+
+
+let foodItems = ["Potato", "Apple", "Litchi", "tomato",];
+
+
+
+
+
+
+
 
 
