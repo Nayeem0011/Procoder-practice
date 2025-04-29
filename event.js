981ef4f025572d3f008a83000cdd80fab2e08480
Sk Nayeem <sknayeemislam384@gmail.com>
2025-04-29 22:21:44 +0600

    Event Add

---
+++ event.js
@@ -0,0 +1,44 @@
+
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () => {
+    console.log("Button Was Cliked");
+}
+
+
+
+
+
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = () => {
+//     console.log("Button Was Cliked");
+//     let val = 25;
+//     val++
+//     console.log(val);
+// }
+
+
+
+
+
+let btn2 = document.querySelector("#btn2");
+
+btn2.ondblclick = () => {
+    console.log("Button Was Cliked");
+}
+
+
+
+
+let box = document.querySelector("div");
+
+box.onmouseover = () => {
+    console.log("You are inside div");
+}
+
+
+
+
+
+
