bf15e1ffb7e7c911704ef4cd4085715a2311984a
Sk Nayeem <sknayeemislam384@gmail.com>
2025-05-05 01:23:03 +0600

    Event_Add

--- event.js
+++ event.js
@@ -1,9 +1,9 @@
 
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () => {
-    console.log("Button Was Cliked");
-}
+// btn1.onclick = () => {
+//     console.log("Button Was Cliked");
+// }
 
 
 
@@ -22,11 +22,11 @@ btn1.onclick = () => {
 
 
 
-let btn2 = document.querySelector("#btn2");
+// let btn2 = document.querySelector("#btn2");
 
-btn2.ondblclick = () => {
-    console.log("Button Was Cliked");
-}
+// btn2.ondblclick = () => {
+//     console.log("Button Was Cliked");
+// }
 
 
 
@@ -44,6 +44,32 @@ btn2.ondblclick = () => {
 
 // Event Object
 
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = (e) => {
+//   console.log(e);
+//   console.log(e.type);
+//   console.log(e.target);
+//   console.log(e.preventDefault());
+//   console.log(e.key);
+//   console.log(e.clientX, e.clientY);
+// }
+
+
+// let btn2 = document.querySelector("#btn2");
+
+// btn2.ondblclick = (e) => {
+//     console.log(e);
+//     console.log(e.type);
+//     console.log(e.target);
+//     console.log(e.preventDefault());
+//     console.log(e.key);
+//     console.log(e.clientX, e.clientY);
+// }
+
+
+
+
 // let box = document.querySelector("div");
 
 // box.onmouseover = (evn) => {
@@ -55,12 +81,70 @@ btn2.ondblclick = () => {
 
 
 
-const box = document.getElementById("box");
-const btn = document.getElementById("btn");
+// Event Lestener
 
-btn.addEventListener("click", () => {
-  box.classList.toggle("blue");
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.addEventListener("click", () => {
+//   console.log("Button Was Cliked");
+// });
+
+// btn1.addEventListener("click", () => {
+//   console.log("Button Was Cliked Handler2");
+// });
+ 
+
+
+
+// Event Lestener Object
+
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.addEventListener("click", (e) => {
+//   console.log("Button Was Cliked");
+//   console.log(e);
+//   console.log(e.type);
+// });
+
+// btn1.addEventListener("click", () => {
+//   console.log("Button Was Cliked Handler2");
+// });
+
+
+
+// Event Lestener Remove
+ 
+let btn1 = document.querySelector("#btn1");
+
+btn1.addEventListener("click", () => {
+  console.log("Button Was Cliked Hanhler1");
 });
+
+const Handler2 = () => {
+  console.log("Button Was Cliked Handler2");
+};
+btn1.addEventListener("click", Handler2);
+ 
+btn1.addEventListener("click", () => {
+  console.log("Button Was Cliked Handler3");
+});
+ 
+btn1.addEventListener("click", () => {
+  console.log("Button Was Cliked Handler4");
+});
+
+btn1.removeEventListener("click", Handler2);
+
+
+
+// Let's Practice
+
+
+// let modeBtn = document.querySelector("#mode");
+
+// modeBtn.addEventListener("click")
+
+
 
 
 
