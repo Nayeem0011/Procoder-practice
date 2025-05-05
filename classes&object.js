b29786714d112deceae6d5353b50ecb52c1b6d93
Sk Nayeem <sknayeemislam384@gmail.com>
2025-05-06 00:50:53 +0600

    Classes&Object_add

---
+++ classes&object.js
@@ -0,0 +1,42 @@
+               //Classes & Object in js
+
+
+const student = {
+    fullName: "Nayeem Islam",
+    age: 22,
+    marks: 84.3,
+    printMarks: function () {
+        console.log("Marks = ", this.marks);
+    }
+}
+console.log(student);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
