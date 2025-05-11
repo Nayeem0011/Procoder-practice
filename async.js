77453c5f74f8d86d96b4a6197dedf77e38ed1f88
Sk Nayeem <sknayeemislam384@gmail.com>
2025-05-11 19:09:04 +0600

    Astnc_Add

--- async.js
+++ async.js
@@ -57,37 +57,146 @@
 // Callbacks Hell
 
 // Nesting if else
-let age = 20;
-if (age >= 18) {
-    if (age >= 50) {
-        console.log("Senior");
-    } else {
-        console.log("Middle");
-    }
-} else {
-    console.log("Chail");
-}
+// let age = 20;
+// if (age >= 18) {
+//     if (age >= 50) {
+//         console.log("Senior");
+//     } else {
+//         console.log("Middle");
+//     }
+// } else {
+//     console.log("Chail");
+// }
 
-// Nesting loop
-for (let i = 0; i < 5; i++) {
-    let str = "";
-    for (let j = 0; j < 5; j++) {
-        str = str + j;
-    }
-    console.log(i, str);
-}
-
-
+// // Nesting loop
+// for (let i = 0; i < 5; i++) {
+//     let str = "";
+//     for (let j = 0; j < 5; j++) {
+//         str = str + j;
+//     }
+//     console.log(i, str);
+// }
 
 
 
+// function getData(dataId) {
+//     setTimeout(() =>{
+//         console.log("Data", dataId);
+//     }, 2000)
+// }
+// getData(1234)
+
+
+
+
+// function getData(dataId, getNextData) {
+//     setTimeout(() =>{
+//         console.log("Data", dataId);
+//         if (getNextData) {
+//             getNextData();
+//         }
+//     }, 2000)
+// }
+// // Callback Hell
+// getData(1, () => {
+//     getData(2);
+// });
+
+
+
+
+// function getData(dataId, getNextData) {
+//     setTimeout(() =>{
+//         console.log("Data", dataId);
+//         if (getNextData) {
+//             getNextData();
+//         }
+//     }, 2000)
+// }
+// // Callback Hell
+// getData(1, () => {
+//     getData(2, () => {
+//         getData(3, () => {
+//             getData(4);
+//         });
+//     });
+// });
+
+
+
+
+// function getData(dataId, getNextData) {
+    // setTimeout(() =>{
+    //     console.log("Data", dataId);
+    //     if (getNextData) {
+    //         getNextData();
+    //     }
+    // }, 2000)
+// }
+// // Callback Hell
+// getData(1, () => {
+//     console.log("Getting Data1...");
+//     getData(2, () => {
+//         console.log("Getting Data2...");
+//         getData(3, () => {
+//             console.log("Getting Data3...");
+//             getData(4, () => {
+//                 console.log("Getting Data End");
+//             });
+//         });
+//     });
+// });
 
 
 
 
 
 
+// Promise 
 
+// let promise = new Promise((resolve, reject) => {
+//     console.log("My first promise");
+//     resolve("Success")
+// });
+
+
+
+// let promise = new Promise((resolve, reject) => {
+//     console.log("My first promise");
+//     reject("Some error")
+// });
+
+
+// Promise
+// function getData(dataId, getNextData) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Data", dataId);
+//             resolve("Success");
+//             if (getNextData) {
+//                 getNextData();
+//             }
+//         }, 5000)
+//     });
+// };
+// let finalVal = getData(123)
+// console.log(finalVal);
+
+
+
+
+function getData(dataId, getNextData) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("error")
+            if (getNextData) {
+                getNextData();
+            }
+        }, 5000)
+    });
+};
+let finalVal = getData(123)
+console.log(finalVal);
 
 
 
